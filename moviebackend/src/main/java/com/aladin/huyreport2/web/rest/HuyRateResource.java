6559839086083b300214be9cd5d9ddc4a1b3d658
huy <huytqhe151216@fpt.edu.vn>
2022-09-08 14:57:23 +0700

    update rateResource

--- moviebackend/src/main/java/com/aladin/huyreport2/web/rest/HuyRateResource.java
+++ moviebackend/src/main/java/com/aladin/huyreport2/web/rest/HuyRateResource.java
@@ -1,6 +1,7 @@
 package com.aladin.huyreport2.web.rest;
 
 import com.aladin.huyreport2.domain.HuyRate;
+import com.aladin.huyreport2.domain.User;
 import com.aladin.huyreport2.repository.HuyRateRepository;
 import com.aladin.huyreport2.repository.search.HuyRateSearchRepository;
 import com.aladin.huyreport2.service.HuyRateService;
@@ -18,6 +19,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.access.AuthorizationServiceException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +27,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,11 +45,8 @@ public class HuyRateResource {
 
     @Value("${jhipster.clientApp.name}")
     private String applicationName;
-
-//    private  HuyRateRepository huyRateRepository;
     @Autowired
     private HuyRateServiceImpl huyRateServiceImpl;
-    private  HuyRateSearchRepository huyRateSearchRepository;
     @Autowired
     private UserService userService;
     private final HuyRateService huyRateService;
@@ -67,8 +68,9 @@ public class HuyRateResource {
         if (huyRate.getId() != null) {
             throw new BadRequestAlertException("A new huyRate cannot already have an ID", ENTITY_NAME, "idexists");
         }
-        huyRate.setUser(userService.getUserWithAuthorities().get());
-
+        User user =userService.getUserWithAuthorities().get();
+        huyRate.setUser(user);
+        huyRate.setDateCreate(Instant.now());
         HuyRate result = huyRateServiceImpl.save(huyRate);
         return ResponseEntity.created(new URI("/api/huy-rates/" + result.getId()))
             .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
@@ -90,7 +92,16 @@ public class HuyRateResource {
         if (huyRate.getId() == null) {
             throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
         }
-        HuyRate result = huyRateService.save(huyRate);
+        Long id1 = userService.getUserWithAuthorities().get().getId();
+        Long id2 = huyRateServiceImpl.findOne(huyRate.getId()).get().getUser().getId();
+        if (id1!=id2){
+            log.info("Can't edit other people's rate");
+            return ResponseEntity.badRequest().body(null);
+        }
+        huyRate.setDateCreate(Instant.now());
+        huyRate.setUser(userService.getUserWithAuthorities().get());
+        HuyRate result = huyRateServiceImpl.save(huyRate);
+        log.info("edit successfully");
         return ResponseEntity.ok()
             .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, huyRate.getId().toString()))
             .body(result);
@@ -132,6 +143,11 @@ public class HuyRateResource {
     @DeleteMapping("/huy-rates/{id}")
     public ResponseEntity<Void> deleteHuyRate(@PathVariable Long id) {
         log.debug("REST request to delete HuyRate : {}", id);
+        User user = userService.getUserWithAuthorities().get();
+        Long id2 = huyRateServiceImpl.findOne(id).get().getUser().getId();
+        if (user.getId()!=id2 && user.getAuthorities().stream().noneMatch(x->x.getName().equals("ADMIN_ROLE"))){
+            throw new AuthorizationServiceException("You are not authorized to delete this rate");
+        }
         huyRateService.delete(id);
         return ResponseEntity.noContent().headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString())).build();
     }
